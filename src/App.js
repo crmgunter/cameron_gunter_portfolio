b9bf71ffc68231ba3031e5258e819cd5fc3fedb4
crmgunter <crmgunter@gmail.com>
2018-04-04 22:56:33 -0400

    added browser router and added a route to index and /about

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
-import styled from 'styled-components'
-import Welcome from './components/Welcome'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import styled from "styled-components";
+import Welcome from "./components/Welcome";
+import About from './components/About'
 const Body = styled.div`
-text-align: center;
-`
+  text-align: center;
+`;
 
 class App extends Component {
   render() {
     return (
-      <Body>
-        <Header />
-          <Welcome/>
-      </Body>
+      <Router>
+        <Body>
+          <Header />
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About}/>
+        </Body>
+      </Router>
     );
   }
 }
